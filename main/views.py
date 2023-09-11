18a3f4940e5b98a8bffe9ae18cb4d4f2526318fa
NuChraee <caesarramadhan.cr05@gmail.com>
2023-09-11 21:21:07 +0700

    Update README

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
+        'appname' : 'Crimson Chest',
         'name': 'Caesar Syahru Ramadhan',
         'class': 'PBP C',
         
