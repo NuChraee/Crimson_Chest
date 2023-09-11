e2f2784c849a139df44ec4aed7c646c412415ad9
NuChraee <caesarramadhan.cr05@gmail.com>
2023-09-11 23:18:15 +0700

    Update models.py

--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
-class Product(models.Model):
+class item(models.Model):
     name = models.CharField(max_length=255)
     date_added = models.DateField(auto_now_add=True)
     price = models.IntegerField()
